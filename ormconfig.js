bb25363464dc35bf95040c94c496cfa9bb0ca2d7
Gustavo Jonas Baltieri <104153413+gjbaltieri@users.noreply.github.com>
2022-10-26 10:34:01 -0300

    Create ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+export default {
+  type: `postgres`,
+  host: `${process.env.PG_HOST}`,
+  port: `${process.env.PG_PORT}`,
+  username: `${process.env.PG_USERNAME}`,
+  password: `${process.env.PG_PASSWORD}`,
+  database: `${process.env.PG_DATABASE}`,
+  entities: [`${process.env.PG_ENTITIES}`],
+  migrations: [`${process.env.PG_MIGRATIONS}`],
+  cli: { migrationsDir: `${process.env.PG_MIGRATIONS_DIR}` },
+  extra: {
+    ssl: true,
+  },
+}
